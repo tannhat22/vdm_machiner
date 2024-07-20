3a8b8ea34fa1441545266e6022509c2211ef4b04
tannhat22 <nguyentannhat2298@gmail.com>
2024-07-20 16:17:45 +0700

    fix lỗi thứ tự dữ liệu công đoạn theo ngày

--- vdm_lk2_machine/vdm_lk2_machine/PC_service_ros.py
+++ vdm_lk2_machine/vdm_lk2_machine/PC_service_ros.py
@@ -626,10 +626,10 @@ class PlcService(Node):
             stageRawData['offtime'].extend(dataHistory['dictType']['offtime'])
 
         k = len(stageRawData['dates'])
-        stageSumData = {}
+        stageSumRaw = {}
         for i in range(0,k):
             keyTime = f"{stageRawData['dates'][i]}-{stageRawData['shift'][i]}"
-            if keyTime not in stageSumData:
+            if keyTime not in stageSumRaw:
                 machineMsg = MachineData()
                 machineMsg.date = stageRawData['dates'][i]
                 machineMsg.shift = stageRawData['shift'][i]
@@ -637,16 +637,18 @@ class PlcService(Node):
                 machineMsg.noload = stageRawData['noload'][i]
                 machineMsg.error = stageRawData['error'][i]
                 machineMsg.offtime = stageRawData['offtime'][i]
-                stageSumData[keyTime] = machineMsg
+                stageSumRaw[keyTime] = machineMsg
             else:
-                stageSumData[keyTime].underload += stageRawData['underload'][i]
-                stageSumData[keyTime].noload += stageRawData['noload'][i]
-                stageSumData[keyTime].error += stageRawData['error'][i]
-                stageSumData[keyTime].offtime += stageRawData['offtime'][i]
+                stageSumRaw[keyTime].underload += stageRawData['underload'][i]
+                stageSumRaw[keyTime].noload += stageRawData['noload'][i]
+                stageSumRaw[keyTime].error += stageRawData['error'][i]
+                stageSumRaw[keyTime].offtime += stageRawData['offtime'][i]
+
+        stageSumFilter = dict(sorted(stageSumRaw.items(), key=lambda item: self.text_to_date(item[0].split('-')[0])))
 
         machineMsg = MachineDataStamped()
         machineMsg.machine_name = request.stage
-        machineMsg.machine_data = list(stageSumData.values())
+        machineMsg.machine_data = list(stageSumFilter.values())
         stageData.append(machineMsg)
         
         response.success = True
--- vdm_lk2_machine/vdm_lk2_machine/test.py
+++ vdm_lk2_machine/vdm_lk2_machine/test.py
@@ -1,8 +1,34 @@
-from mcprotocol.type1e import Type1E
+# from mcprotocol.type1e import Type1E
 
-pyPLC = Type1E('F')
-pyPLC.connect('192.168.1.251', 8000)
-print("Connect success")
-a = pyPLC.batchread_wordunits('D700', 7)
-print(len(a))
-print('data: ',a)
+# pyPLC = Type1E('F')
+# pyPLC.connect('192.168.1.251', 8000)
+# print("Connect success")
+# a = pyPLC.batchread_wordunits('D700', 7)
+# print(len(a))
+# print('data: ',a)
+
+from datetime import datetime
+
+# Giả sử bạn có một dictionary như sau
+data = {
+    '10/07/2024-DAY': 'value1',
+    '25/02/2024-NIGHT': 'value2',
+    '23/08/2024-DAY': 'value3',
+    '30/08/2024-DAY': 'value3',
+    '01/03/2024-DAY': 'value3',
+    '30/05/2024-DAY': 'value3',
+    '22/03/2024-DAY': 'value3',
+    '30/12/2024-DAY': 'value3',
+    '30/11/2024-DAY': 'value3',
+}
+
+# Hàm để tách và chuyển đổi ngày
+def extract_date(key):
+    date_str = key.split('-')[0]  # Lấy phần ngày
+    return datetime.strptime(date_str, '%d/%m/%Y')
+
+# Sắp xếp dictionary
+sorted_data = dict(sorted(data.items(), key=lambda item: extract_date(item[0])))
+
+# In ra kết quả
+print(sorted_data)
